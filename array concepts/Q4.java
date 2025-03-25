31a7c65f2aeb0d4ded24a2e398b8992dfb1c6a0e
apurv12-ai <lbsnaaapurv1225@gamil.com>
2025-03-25 08:30:23 +0530

    Adding initial file

---
+++ array concepts/Q4.java
@@ -0,0 +1,36 @@
+/*Write a program to store multiple values in an array up to a maximum of 10 or until the user enters a 0 or a negative number. Show all the numbers as well as the sum of all numbers
+Hint =>
+
+Create a variable to store an array of 10 elements of type double as well as a variable to store the total of type double initializes to 0.0. Also, the index variable is initialized to 0 for the array
+Use infinite while loop as in while (true)
+Take the user entry and check if the user entered 0 or a negative number to break the loop
+Also, break from the loop if the index has a value of 10 as the array size is limited to 10.
+If the user entered a number other than 0 or a negative number inside the while loop then assign the number to the array element and increment the index value
+Take another for loop to get the values of each element and add it to the total
+Finally display the total value */
+import java.util.Scanner;
+public class Q4 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        double[] arr = new double[10];
+        double total = 0.0;
+        int index = 0;
+        while(true){
+            System.out.println("Enter a number : ");
+            double num = scn.nextDouble();
+            if(num<=0){
+                break;
+            }
+            if(index==10){
+                break;
+            }
+            arr[index] = num;
+            index++;
+        }
+        for(int i=0;i<index;i++){
+            total+=arr[i];
+            System.out.println("Number "+(i+1)+" : "+arr[i]);
+        }
+        System.out.println("Total : "+total);
+    }
+}
